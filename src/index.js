68c336e95d0b178392ee71f7de1065cddee7fb1a
Micah Caffey <mrcaffey@gmail.com>
2018-09-29 15:45:42 -0600

    added simple click handler

--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let model= { clicks: 0 };
+
+function render () { //when model changes, it should refresh interface
+ReactDOM.render(<App
+   clicks={model.clicks} 
+   onClick={()=> {model.clicks +=1; render(); }} />, document.getElementById('root'));
+}
+render();
 registerServiceWorker();
